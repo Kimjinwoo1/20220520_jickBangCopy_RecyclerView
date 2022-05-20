43d5dbbc1e8341bb60b5ad6237fb1c4878af0419
smlee <larc21c@gmail.com>
2022-05-20 11:04:43 +0900

    2. recyclerView 에 배치할 한칸 xml 작성

---
+++ app/src/main/java/com/nepplus/a20220520_jickbangcopy_recyclerview/romm_list_item.kt
@@ -0,0 +1,11 @@
+package com.nepplus.a20220520_jickbangcopy_recyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class romm_list_item : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_romm_list_item)
+    }
+}
